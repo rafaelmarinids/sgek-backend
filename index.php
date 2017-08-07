f8b22562081707a786e82dbbbccfb566e0bf9729
Rafael Marini <rafaelmarinids@gmail.com>
2017-08-06 22:35:49 -0300

    Alterada a localização do frontcontroller.

--- index.php
+++ index.php
@@ -19,22 +19,22 @@ header('Access-Control-Expose-Headers: Origin, X-Requested-With, Content-Type, A
 
 session_start();
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/src/autoload.php';
 
 // Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require __DIR__ . '/src/settings.php';
 
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
+require __DIR__ . '/src/dependencies.php';
 
 // Register middleware
-require __DIR__ . '/../src/middleware.php';
+require __DIR__ . '/src/middleware.php';
 
 // Register routes
-require __DIR__ . '/../src/routes.php';
+require __DIR__ . '/src/routes.php';
 
 // Run app
 $app->run();
